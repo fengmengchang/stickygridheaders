ebc3e0208321877d3123eae106cf55633ae81387
Xenon Chau <cto@ido-game.com>
2016-11-22 17:57:12 +0800

    stickygridheaders

---
+++ stickygridheaders/gen/com/tonicartos/widget/stickygridheaders/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tonicartos.widget.stickygridheaders;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
